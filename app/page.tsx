9083beb08d7fc639a47f031073274c87c737dde0
Lucas O'Brien <lucasobrien@lucass-mbp.lan>
2026-05-11 17:22:38 -0700

    Tighten home grid: dense flow + matched row spans for bento layout

--- app/page.tsx
+++ app/page.tsx
@@ -18,23 +18,23 @@ export default function HomePage() {
     <>
       <Header />
       <main className="mx-auto max-w-[1400px] px-4 pb-10 pt-24 md:px-6 md:pt-28">
-        <section className="grid auto-rows-auto grid-cols-1 gap-3 md:grid-cols-2 md:gap-4 xl:grid-cols-6 xl:gap-4 xl:items-stretch">
+        <section className="grid auto-rows-auto grid-cols-1 gap-3 md:grid-cols-2 md:gap-4 xl:grid-cols-6 xl:gap-4 xl:items-stretch xl:grid-flow-dense">
           <HeroTile span="md:col-span-2 xl:col-span-3 xl:row-span-2" />
           <GithubTile span="md:col-span-2 xl:col-span-2" />
-          <ExperienceTile span="xl:col-span-1" />
+          <ExperienceTile span="md:col-span-2 xl:col-span-1" />
 
-          <WorkAccordionTile span="md:col-span-2 xl:col-span-2" />
-          <SkillsTile span="md:col-span-2 xl:col-span-2" />
-          <AboutTile span="md:col-span-2 xl:col-span-2" />
+          <WorkAccordionTile span="md:col-span-2 xl:col-span-3 xl:row-span-2" />
+          <SkillsTile span="md:col-span-2 xl:col-span-3 xl:row-span-2" />
+          <AboutTile span="md:col-span-2 xl:col-span-3" />
 
           <WakaTimeTile span="md:col-span-2 xl:col-span-2" />
           <EducationTile span="md:col-span-2 xl:col-span-2" />
-          <SpotifyTile span="md:col-span-2 xl:col-span-2" />
+          <SpotifyTile span="md:col-span-2 xl:col-span-2 xl:row-span-2" />
 
-          <ProjectsTile span="md:col-span-2 xl:col-span-3" />
-          <GlobeTile span="md:col-span-2 xl:col-span-3" />
+          <ProjectsTile span="md:col-span-2 xl:col-span-4" />
 
-          <PhotosTile span="md:col-span-2 xl:col-span-6" />
+          <GlobeTile span="md:col-span-2 xl:col-span-3 xl:row-span-2" />
+          <PhotosTile span="md:col-span-2 xl:col-span-3 xl:row-span-2" />
         </section>
       </main>
       <Footer />
